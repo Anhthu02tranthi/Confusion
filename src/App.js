10f775b53237e0105f2a5ce19276a7709ec923ae
Thu <thuttade160388@fpt.edu.vn>
2023-06-07 11:20:47 +0700

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          DDay la ung dung react
         </p>
         <a
           className="App-link"
